1cc2ef414ee31e33f2db4ffd745dc717146a8489
Antoine Rozo <antoine.rozo@gmail.com>
2014-01-10 17:56:45 +0100

    post.Post model

--- rodolphe/post/models.py
+++ rodolphe/post/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    active = models.BooleanField(default=True)
+    parent = models.ForeignKey('Post', blank=True, null=True)
+    content = models.TextField()
+    hash_id = models.IntegerField()
+
+    def __str__(self):
+        return '#{}'.format(self.id)
