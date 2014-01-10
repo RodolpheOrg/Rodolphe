5473a7e56f4809228cd23d03553d2d9e4eec698c
Antoine Rozo <antoine.rozo@gmail.com>
2014-01-10 21:51:59 +0100

    UUIDField

--- rodolphe/post/models.py
+++ rodolphe/post/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+from rodolphe.fields import UUIDField
+
 # Create your models here.
 
 class Post(models.Model):
+    uuid = UUIDField()
     title = models.CharField(max_length=100, blank=True)
     active = models.BooleanField(default=True)
     parent = models.ForeignKey('Post', blank=True, null=True)
     content = models.TextField()
-    hash_id = models.BigIntegerField()
+    #hash_id = models.BigIntegerField()
 
     def __str__(self):
         return '#{}'.format(self.id)
---
+++ rodolphe/rodolphe/fields.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from uuid import UUID
+
+class UUIDField(models.BinaryField, metaclass=models.SubfieldBase):
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 16
+        super().__init__(*args, **kwargs)
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs['max_length']
+        return name, path, args, kwargs
+    def to_python(self, value):
+        return UUID(bytes=value)
+    def get_prep_value(self, value):
+        return super().get_prep_value(value.bytes)
