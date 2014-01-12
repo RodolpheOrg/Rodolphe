ae5f1953bed7398a7f3d1a5da22b8f24f6ff05b0
Antoine Rozo <antoine.rozo@gmail.com>
2014-01-12 16:35:09 +0100

    Adding tags in markdown (strike, underline, post references)

--- rodolphe/utils/templatetags/markup.py
+++ rodolphe/utils/templatetags/markup.py
@@ -7,16 +7,36 @@ import markdown
 register = template.Library()
 
 
-class DisableImages(markdown.Extension):
+class DisableImagesExtension(markdown.Extension):
     def extendMarkdown(self, md, md_globals):
         del md.inlinePatterns['image_link']
         del md.inlinePatterns['image_reference']
+disable_images = DisableImagesExtension()
 
+class MoreStyleExtension(markdown.Extension):
+    def extendMarkdown(self, md, md_globals):
+        del_tag = markdown.inlinepatterns.SimpleTagPattern(r'(--)(.*?)--', 'del')
+        md.inlinePatterns.add('del', del_tag, '>not_strong')
+        u_tag = markdown.inlinepatterns.SimpleTagPattern(r'(__)(.*?)__', 'u')
+        md.inlinePatterns.add('underline', u_tag, '>del')
+more_style = MoreStyleExtension()
+
+class PostReferencesPattern(markdown.inlinepatterns.Pattern):
+    def handleMatch(self, m):
+        a = markdown.util.etree.Element('a')
+        a.set('href', '#p{}'.format(m.group(3)))
+        a.text = '#{}'.format(m.group(3))
+        return a
+class PostReferencesExtension(markdown.Extension):
+    def extendMarkdown(self, md, md_globals):
+        pattern = PostReferencesPattern(r'(&#)([0-9]+)')
+        md.inlinePatterns.add('post_references', pattern, '_end')
+post_references = PostReferencesExtension()
 
 @register.filter('markdown', is_safe=True)
 @stringfilter
 def do_markdown(value):
-    extensions = ('nl2br', DisableImages())
+    extensions = ('nl2br', disable_images, more_style, post_references)
     return mark_safe(markdown.markdown(value,
                                        extensions,
                                        safe_mode='escape',
