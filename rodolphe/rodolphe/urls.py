316c4757f88c86b656eef08aac9f47514c085dc2
Antoine Rozo <antoine.rozo@gmail.com>
2014-05-22 00:08:06 +0200

    robots.txt

--- rodolphe/rodolphe/urls.py
+++ rodolphe/rodolphe/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -22,4 +23,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^captcha/', include('captcha.urls')),
+
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt',
+                                               content_type='text/plain'))
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
